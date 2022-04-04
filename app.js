b899173592586ec940e4a79012ed6f6e0fb71ae0
sherryrich <ricsherry@gmail.com>
2022-04-04 08:57:11 +0000

    css styling loaded

--- app.js
+++ app.js
@@ -13,25 +13,47 @@ const getData = () => [
     { imgSrc: "./images/whatsapp.png", name: "whatsapp" },
     { imgSrc: "./images/weather.png", name: "weather" },
     { imgSrc: "./images/duolingo.png", name: "duolingo" },
-    { imgSrc: "./images/app-store.png", name: "app-store" },
+    { imgSrc: "./images/maps.png", name: "maps" },
     { imgSrc: "./images/deliveroo.png", name: "deliveroo" },
     { imgSrc: "./images/duolingo.png", name: "duolingo" },
     { imgSrc: "./images/nike.png", name: "nike" },
-    { imgSrc: "./images/instagram.png", name: "instagram"},
+    { imgSrc: "./images/instagram.png", name: "instagram" },
+    { imgSrc: "./images/whatsapp.png", name: "whatsapp" },
+    { imgSrc: "./images/weather.png", name: "weather" },
+    { imgSrc: "./images/duolingo.png", name: "duolingo" },
+    { imgSrc: "./images/maps.png", name: "maps" },
+    { imgSrc: "./images/deliveroo.png", name: "deliveroo" },
+    { imgSrc: "./images/duolingo.png", name: "duolingo" },
+    { imgSrc: "./images/nike.png", name: "nike" },
+    { imgSrc: "./images/instagram.png", name: "instagram" },
 ];
 
 
 //Randomize
-    const randomize = () => {
+const randomize = () => {
     const cardData = getData();
     cardData.sort(() => Math.random() - 0.5);
     return cardData;
 };
 
 // Card Generator Function
-    const cardGenerator = () => {
+const cardGenerator = () => {
     const cardData = randomize();
-    console.log(cardData);
+    //Generate the HTML
+    cardData.forEach((item) => {
+        const card = document.createElement("div");
+        const face = document.createElement("img");
+        const back = document.createElement("div");
+        card.classList = "card";
+        face.classList = "face";
+        back.classList = "back";
+        //attach the info to the cards
+        face.src = item.imgSrc;
+        //attach the card to the section
+        sections.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+    });
 };
 
 cardGenerator();
