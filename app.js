83265e20230402781efb7ce69505730feb1d0b98
sherryrich <ricsherry@gmail.com>
2022-04-04 08:13:07 +0000

    random generator function working

---
+++ app.js
@@ -0,0 +1,33 @@
+//Grab a couple of things
+const sections = document.querySelector("section");
+const playerLivesCount = document.querySelector("span");
+const playerLives = 6;
+
+//Link text
+playerLivesCount.textContent = playerLives;
+
+//Generate the data
+[{ imgSrc: "./images/whatsapp.png" }]
+
+const getData = () => [
+    { imgSrc: "./images/whatsapp.png", name: "whatsapp" },
+    { imgSrc: "./images/weather.png", name: "weather" },
+    { imgSrc: "./images/duolingo.png", name: "duolingo" },
+    { imgSrc: "./images/app-store.png", name: "app-store" },
+];
+
+
+//Randomize
+    const randomize = () => {
+    const cardData = getData();
+    cardData.sort(() => Math.random() - 0.5);
+    return cardData;
+};
+
+// Card Generator Function
+    const cardGenerator = () => {
+    const cardData = randomize();
+    console.log(cardData);
+};
+
+cardGenerator();
